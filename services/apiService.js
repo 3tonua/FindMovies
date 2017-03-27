03cc1fcce454f1c5af6b28e25c10521c7ff7109c
3tonua <3ton.ua@gmail.com>
2017-03-27 16:21:35 +0300

    some changes in header

--- services/apiService.js
+++ services/apiService.js
@@ -1,13 +1,13 @@
-app.service ('API', function ($http, $q) {
+app.service ('API', function ($http, $q, apiKeys) {
     return {
         getMostPopularMovies: function () {
-            var key = 'aa1625f9990899f7cfc5676de6cf520d';
+            // var key = 'aa1625f9990899f7cfc5676de6cf520d';
             var d = $q.defer();
             $http({
                 method: 'GET',
                 url: 'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22',
                 params: {
-                    api_key: key
+                    api_key: apiKeys.tmdb
                 }
             }).then(function (data) {
                 console.log(data);
