c20acf2334cadf6738f5e650a7b1ad7794cbc8d2
3tonua <3ton.ua@gmail.com>
2017-04-05 20:25:20 +0300

    some changes

--- controllers/allMoviesController.js
+++ controllers/allMoviesController.js
@@ -1,5 +1,4 @@
-app.controller('allMoviesController', function ($scope, API) {
-    // $scope.movies = [];
+app.controller('allMoviesController', function ($scope, API, $routeProvider) {
     API.getMostPopularMovies().then(function (movies) {
         console.log(movies);
         console.log(movies.results);
