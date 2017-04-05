a14e58aa908848bb31663571846793caade274be
3tonua <3ton.ua@gmail.com>
2017-04-05 18:36:35 +0300

    some changes

--- controllers/tvSerialsController.js
+++ controllers/tvSerialsController.js
@@ -2,6 +2,7 @@ app.controller('tvSerialsController', function ($scope, API) {
     API.getSerials().then(function (serials) {
         console.log(serials);
         console.log(serials.results);
-        $scope.serials = serials.results
+        $scope.serials = serials.results;
+        $scope.pages = serials
     });
 });
