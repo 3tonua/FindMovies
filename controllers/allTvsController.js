ebc42a9b5b9955953af5b7e03c3696362f7526e0
3tonua <3ton.ua@gmail.com>
2017-04-12 17:31:31 +0300

    optimisation

--- controllers/allTvsController.js
+++ controllers/allTvsController.js
@@ -1,10 +1,30 @@
 app.controller('allTvsController', function ($scope, API) {
-    API.getTvs().then(function (serials) {
-        console.log(serials);
-        console.log(serials.results);
-        $scope.serials = serials.results;
-        $scope.pages = serials
+
+    $scope.counter = 1;
+    $scope.url = 'https://api.themoviedb.org/3/discover/tv';
+    console.log($scope.url);
+    API.getTvs($scope.url, $scope.counter).then(function (serials) {
+        // console.log(serials.results);
+        $scope.serials = serials.results
     });
+
+    $scope.countplus = function () {
+        $scope.counter = $scope.counter + 1;
+        console.log($scope.url, $scope.counter);
+        API.getTvs($scope.url, $scope.counter).then(function (serials) {
+            // console.log(serials.results);
+            $scope.serials = serials.results
+        });
+    };
+
+    $scope.countminus = function () {
+        $scope.counter = $scope.counter - 1;
+        console.log($scope.url, $scope.counter);
+        API.getTvs($scope.url, $scope.counter).then(function (serials) {
+            // console.log(serials.results);
+            $scope.serials = serials.results
+        });
+    };
 
     $scope.watchlistCount = localStorage.length;
 });
